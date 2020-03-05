882caa3e9d151657f61d1f7fcdab145ae6339c3b
Mauricio Milano <mauricio.milano@.ibm.com>
2020-03-05 16:54:08 -0300

    primeiro Commit

---
+++ src/components/buttons/buttons.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './buttons.css';
+
+function button() {
+    return (
+        <div class="buttons">
+            <i class="fab fa-linkedin"></i>
+            <i class="fas fa-phone"></i>
+            <i class="fas fa-at"></i>
+        </div>
+    );
+}
+
+export default button;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import button from './components/buttons/buttons'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(button(),document.getElementById("buttons"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
